e4ebd933a9d7a3ee0e0a6e126d4ecb74366c7376
Diego Hernández <dieherram@gmail.com>
2020-10-16 21:18:04 -0300

    actualización

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,26 +1,35 @@
 <template>
   <div>
-      <h2>Busca tu juego</h2>
-      <input type="text" v-model="searchId">
-      <ul>
-          <li v-for="game in searchByIdGames" :key="game.id">
-              {{game.id}}|{{game.name}}|{{game.stock}}|{{game.price}}
-          </li>
-      </ul>
+      <h2>{{totalStock}}</h2>
+      <list :games="availableGames"></list>
+    <h2>Busca tu juego</h2>
+    <input type="text" v-model="searchId" />
+    <ul>
+      <li v-for="game in searchByIdGames" :key="game.id">
+        {{ game.id }}|{{ game.name }}|{{ game.stock }}|{{ game.price }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import{
-    mapGetters, mapState
-} from "vuex"
+import List from "@/components/List";
+import { mapGetters} from "vuex";
 export default {
-    data() {
-        return {searchId:""}
-    }
-}
+  data() {
+    return { searchId: "" };
+  },
+  components: {
+      List
+  },
+  computed: {
+    ...mapGetters(["availableGames","searchById", "totalStock"]),
+    searchByIdGames() {
+      return this.searchById(this.searchId)
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/store.js
+++ src/store.js
@@ -9,7 +9,17 @@ const store = new Vuex.Store({
     subtitle: "Juegos de PC y Consolas",
     games: [{id:"0001", name:"Sekiro", stock:100, price:30000, color:"red", outstanding:false},{id:"0002", name:"Fifa 21", stock:100, price:25000, color:"blue", outstanding:true},{id:"0003", name:"Gears of War 4", stock:100, price:15000, color:"green", outstanding:true},{id:"0004", name:"Mario Tennis Aces", stock:100, price:35000, color:"yellow", outstanding:false},{id:"0005", name:"Bloodhorne", stock:100, price:10000, color:"blue", outstanding:false},{id:"0006", name:"Forza Horizon 4", stock:100, price:20000, color:"red", outstanding:true}]
   },
-  getters: {},
+  getters: {
+    availableGames(state){
+      return state.games.filter(game => game.stock > 0);
+    },
+    searchById:(_state, getters) => (id) => {
+    return getters.availableGames.filter(game => game.id == id)
+    },
+    totalStock(state){
+    return state.games.reduce((acc, game) => acc + game.stock, 0)
+    }
+  },
   mutations: {},
   actions: {}
 });
